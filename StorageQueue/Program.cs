52271a4b9be195a4fd77e13975bb60f755c7f054
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-01-02 19:30:19 +0100

    PeekMessages

--- StorageQueue/Program.cs
+++ StorageQueue/Program.cs
@@ -1,10 +1,28 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 
 string connectionString = "";
 string queueName = "appqueue";
 
-SendMessage("Test message 1");
-SendMessage("Test message 2");
+//SendMessage("Test message 1");
+//SendMessage("Test message 2");
+
+PeekMessages();
+
+void PeekMessages()
+{
+    QueueClient queueClient = new(connectionString, queueName);
+    int maxNumberOfMessagesToGetFromQueue = 10;
+
+    PeekedMessage[] peekedMessages = queueClient.PeekMessages(10);
+
+    Console.WriteLine("The messages in the queue are:");
+
+    foreach (var message in peekedMessages)
+    {
+        Console.WriteLine(message.Body);
+    }
+}
 
 void SendMessage(string message)
 {
