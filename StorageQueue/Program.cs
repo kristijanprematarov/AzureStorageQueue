34c588b61c35521e50c6917db8da98c5c00ed9aa
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-01-02 19:27:12 +0100

    sending messages

--- StorageQueue/Program.cs
+++ StorageQueue/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Azure.Storage.Queues;
+
+string connectionString = "";
+string queueName = "appqueue";
+
+SendMessage("Test message 1");
+SendMessage("Test message 2");
+
+void SendMessage(string message)
+{
+    QueueClient queueClient = new(connectionString, queueName);
+
+    if (queueClient.Exists())
+    {
+        queueClient.SendMessage(message);
+        Console.WriteLine("Message was sent");
+    }
+    else
+    {
+        Console.WriteLine("Failed to send message");
+    }
+}
